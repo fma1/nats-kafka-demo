2a323ff28c31d95b2b2e330b68a39fae7ece138c
Fred Ma <fredma1234@gmail.com>
2021-04-22 22:05:18 -0400

    Got the workflow working from Kafka Producer->Kafka Streams->Kafka Consumer Switched out tweets.json with fresh tweets

--- src/main/scala/NatsKafkaDemo.scala
+++ src/main/scala/NatsKafkaDemo.scala
@@ -1,19 +1,18 @@
 import com.danielasfregola.twitter4s.entities.Tweet
 import com.danielasfregola.twitter4s.processors.TwitterProcessor.{json4sFormats, serialization => theSerialization}
-import com.fasterxml.jackson.databind.{DeserializationFeature, JsonNode, ObjectMapper, PropertyNamingStrategy}
 import io.github.azhur.kafkaserdejson4s.Json4sSupport
 import io.nats.client.{Connection, Dispatcher, MessageHandler, Nats}
-import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, KafkaConsumer}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.kafka.common.serialization.Serdes.StringSerde
-import org.apache.kafka.common.serialization.{Serde, Serdes, StringDeserializer, StringSerializer}
-import org.apache.kafka.connect.json.{JsonDeserializer, JsonSerializer}
+import org.apache.kafka.common.serialization.{Serde, StringDeserializer, StringSerializer}
 import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
 import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.Consumed._
 import org.json4s.{Formats, Serialization}
 import org.slf4j.{Logger, LoggerFactory}
 
+import java.time.Duration
 import java.util.Properties
 import scala.compat.java8.DurationConverters.FiniteDurationops
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -21,14 +20,17 @@ import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 import scala.io.{BufferedSource, Source}
 import scala.jdk.CollectionConverters._
+import scala.util.{Failure, Success}
 
 object NatsKafkaDemo {
-  val logger: Logger = LoggerFactory.getLogger("NatsKafkaDemo")
+  private[this] val logger: Logger = LoggerFactory.getLogger("NatsKafkaDemo")
 
-  val BOOTSTRAP_SERVER = "127.0.0.1:9092"
-  val FROM_TOPIC = "tweets"
-  val TO_TOPIC = "retweets"
-  val GROUP_ID = "nats-kafka-demo-1"
+  private[this] val BOOTSTRAP_SERVER = "127.0.0.1:9092"
+  private[this] val FROM_TOPIC = "tweets-2"
+  private[this] val TO_TOPIC = "retweets-2"
+  private[this] val GROUP_ID = "nats-kafka-demo-1"
+  private[this] val TIMEOUT_MILLS = 100
+  private[this] val APPLICATION_ID = "streams-filter-tweets"
 
   val producerProps = new Properties()
   producerProps.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER)
@@ -44,7 +46,7 @@ object NatsKafkaDemo {
 
   val streamsProps: Properties = {
     val p = new Properties()
-    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-filter-tweets")
+    p.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID)
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER)
     p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, classOf[StringSerde])
     p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, classOf[StringSerde])
@@ -53,35 +55,44 @@ object NatsKafkaDemo {
 
   implicit val serialization: Serialization = theSerialization
   // val serde: Serde[Tweet] = Json4sSupport.toSerde
-  // val serde: Serde[String] = new StringSerde()
-
-  val jsonObjectMapper = new ObjectMapper
-  val jsonSerde: Serde[JsonNode] = Serdes.serdeFrom(new JsonSerializer(), new JsonDeserializer())
-  jsonObjectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE)
-  jsonObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  val serde: Serde[String] = new StringSerde()
 
   def main(args: Array[String]): Unit = {
     // Kafka Stuff
     val builder = new StreamsBuilder
-
-    builder.stream(FROM_TOPIC, Consumed.`with`(jsonSerde, jsonSerde))
-      .filter((_, jsonNode) => {
-        System.err.println(jsonNode)
-        jsonNode.get("retweeted").asBoolean()
+    builder.stream(FROM_TOPIC, `with`(serde, serde))
+      .filter((_, tweetJson) => {
+        logger.info(s"Tweet JSON: $tweetJson")
+        val tweet = serialization.read[Tweet](tweetJson)
+        tweet.text.contains("RT @")
       })
       .to(TO_TOPIC)
 
     val producer = new KafkaProducer[String, String](producerProps)
     val consumer = new KafkaConsumer[String, String](consumerProps)
     val streams = new KafkaStreams(builder.build(), streamsProps)
-    consumer.subscribe(List(TO_TOPIC).asJava)
+    consumer.subscribe(List(TO_TOPIC).asJavaCollection)
     streams.start()
 
     Future {
-      while (true) consumer.poll((100 milliseconds).toJava).asScala.foreach(record => {
-        logger.info(s"Key: ${record.key()}, Value: ${record.value()}")
-        logger.info(s"Partition: ${record.partition()}, Offset: ${record.offset()}")
-      })
+      while (true) {
+        val records = consumer.poll(Duration.ofMillis(TIMEOUT_MILLS))
+        records.iterator().forEachRemaining { record: ConsumerRecord[String, String] =>
+          logger.info(
+            s"""
+               |message
+               |  offset=${record.offset}
+               |  partition=${record.partition}
+               |  key=${record.key}
+               |  value=${record.value}
+           """.stripMargin)
+        }
+      }
+    } onComplete {
+      case Success(_) =>
+        logger.info(s"Successfully processed all records in topic $TO_TOPIC!")
+      case Failure(exception) =>
+        logger.error(s"Error processing records in topic $TO_TOPIC!", exception)
     }
 
     // NATS stuff
@@ -100,7 +111,6 @@ object NatsKafkaDemo {
       })
     dispatcher.subscribe(FROM_TOPIC)
 
-    /*
     val source: BufferedSource = Source.fromURL(getClass.getResource("tweets.json"))
     try {
       source
@@ -109,8 +119,6 @@ object NatsKafkaDemo {
     } finally {
       source.close()
     }
-
-     */
 
     Thread.sleep(20000)
   }
